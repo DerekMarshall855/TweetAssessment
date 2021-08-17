88b3196cbb980c02d31953602e73210cc9b5afcc
DerekMarshall855 <derekmarshall855@gmail.com>
2021-08-17 12:22:13 -0400

    Finished tweet router, start chat socket

--- routes/tweetRouter.js
+++ routes/tweetRouter.js
@@ -19,8 +19,30 @@ tweetRouter.post("/", expressAsyncHandler( async(req, res) => {
 // Get posts
 
 tweetRouter.get("/:id", expressAsyncHandler( async( req, res) => {
-    // Do after (Very late) commit
+    const tweet = await Tweet.findById(req.params.id)
+        .catch(err => {
+            res.status(500).send({success: false, error: err});
+            return;
+        });
+    res.status(200).send({success: true, message: tweet});
 }));
+
+// Get posts that user follows (timeline)
+
+tweetRouter.get("/timeline", expressAsyncHandler( async(req, res) => {
+    try {
+        const currentUser = await User.findById(req.body._id);
+        const tweets = await Tweet.findById({ userId: currentUser._id });
+        const followingTweets = await Promise.all(  // Promise all takes list of promises and returns list of results
+            currentUser.following.map((followId) => {  // use .map to loop through all userId in following array (followId), then create promise (Post.find) to fetch that users tweets
+                return Post.find({ userId: followId });
+            })
+        );
+        res.status(200).send({success: true, message: tweets.concat(...followingTweets)});
+    } catch (err) {
+        res.status(500).send({success: false, error: err});
+    }
+}))
 
 // Update tweet
 
@@ -56,6 +78,20 @@ tweetRouter.delete("/:id", expressAsyncHandler( async(req, res) => {
 
 // Like & Dislike tweet
 
-// Get posts that user follows (timeline)
+tweetRouter.put("/:id/like", expressAsyncHandler( async(req, res) => {
+    const tweet = await Tweet.findById(req.body._id)
+        .catch(err => {
+            res.status(500).send({success: false, error: err});
+            return;
+        });
+    if (!tweet.likes.includes(req.body._id)) {  // Tweet not liked, add id to likes array
+        await tweet.updateOne({ $push: { likes: req.body._id }});
+        res.status(200).send({success: true, message: "Tweet has been liked"});
+    } else {  // Tweet liked, remove id from likes array
+        await tweet.updateOne({ $pull: { likes: req.body._id }});
+        res.status(200).send({success: true, message: "Tweet has been unliked"});
+    }
+}))
+
 
 export default tweetRouter;
